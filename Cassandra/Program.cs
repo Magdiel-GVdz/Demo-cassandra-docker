3237bc7c6da92cdc5b6bb3c90ea68969b3f3df79
Magdiel-GVdz <magdiel.gvdz@gmail.com>
2023-12-28 18:34:14 -0600

    Agregar archivos de proyecto.

---
+++ Cassandra/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cassandra;
+
+namespace CassandraDatabaseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder cassandraBuilder = Cluster.Builder();
+            cassandraBuilder.AddContactPoint("127.0.0.2");
+            var cluster = cassandraBuilder.Build();
+            ISession session = cluster.Connect("demo");
+            RowSet rowSet = session.Execute("select * from users");
+            foreach (var row in rowSet)
+            {
+                Console.WriteLine(row[0]);
+            }
+
+            // Esperar a que el usuario presione una tecla antes de salir
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
+
